4728dc0e8d56da143e2d52ba02dd7d9ad40da0a1
Tsung-Ping <40154075+Tsung-Ping@users.noreply.github.com>
2020-10-13 11:10:37 +0800

    Update

--- Chord_Jazzification.py
+++ Chord_Jazzification.py
@@ -13,7 +13,7 @@ def main():
     # cross_validate_end2end_chord_jazzification(data_dir, hp) # train the end-to-end chord jazzification model with cross-validation sets
     # train_chord_jazzification(data_dir, hp) # train the chord jazzification model with the whole dataset
 
-    chord_jazzification_inference(hp, threshold=0.6, user_input=True, random_sample=True) # generate jazzified chord sequences from the JAAH dataset using the chord jazzification model trained on the chord jazzificaion dataset
+    chord_jazzification_inference(hp, threshold=0.6, user_input=True, random_sample=False) # generate jazzified chord sequences from the JAAH dataset using the chord jazzification model trained on the chord jazzificaion dataset
 
 if __name__ == '__main__':
 
